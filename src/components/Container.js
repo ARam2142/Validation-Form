9d01c14b1ff58c909b0f60aa51630ad25fbffdef
Aram2142 <andresaaramirez@aol.com>
2021-05-21 22:08:25 -0400

    starting out with form

---
+++ src/components/Container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Container = (props) => {
+
+    return (
+        <div>
+            <container>
+                {props.children}
+            </container>
+        </div>
+
+    )
+}
+export default Container
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// //import Form from 'react-bootstrap/Form'
+
+// const Form = (props) => {
+//     return (
+//         <div>
+//             {props.children}
+//         </div>
+//     )
+// }
+// export default Form
